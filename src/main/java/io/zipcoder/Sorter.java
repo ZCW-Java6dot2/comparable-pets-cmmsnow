5ff7f014e263ca4c43bbb36d0e314471fb63b21d
christine <christine@zipcodes-MacBook-Pro-10.local>
2020-11-13 13:19:27 -0500

    set up sorter class

--- src/main/java/io/zipcoder/Sorter.java
+++ src/main/java/io/zipcoder/Sorter.java
@@ -3,9 +3,20 @@ package io.zipcoder;
 import java.util.ArrayList;
 import java.util.Comparator;
 
+/**
+ * Sort objects by type, break ties by name.
+ *                ~ then ~
+ * Sort objects by name, break ties by type.
+ */
+
 public class Sorter implements Comparator<Pet> {
     Console console = new Console();
     ArrayList<Pet> pets = console.getPetsCoven();
+
+    @Override
+    public int compare(Pet o1, Pet o2) {
+        return 0;
+    }
 
     public ArrayList<Pet> compareByType(ArrayList<Pet> input){
         /*Pet pet1, pet2;
@@ -18,10 +29,5 @@ public class Sorter implements Comparator<Pet> {
 
     public ArrayList<Pet> compareByName(ArrayList<Pet> input){
         return null;
-    }
-
-    @Override
-    public int compare(Pet o1, Pet o2) {
-        return 0;
     }
 }
